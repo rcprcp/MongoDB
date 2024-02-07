b87d1997905107a87736eedc5bf06f5a978d7246
Bob Plotts <robertcplotts@gmail.com>
2024-02-07 11:09:14 -0500

    Reformat

--- src/main/java/com/cottagecoders/Mongodb.java
+++ src/main/java/com/cottagecoders/Mongodb.java
@@ -26,14 +26,22 @@ public class Mongodb {
 
     Mongodb mdb = new Mongodb();
     mdb.run();
-
   }
 
   void run() {
-    String connectionString = String.format("mongodb+srv://%s:%s@%s.mongodb.net/?retryWrites=true&w=majority", System.getenv("MONGODB_USERNAME"), System.getenv("MONGODB_PASSWORD"), System.getenv("MONGODB_CLUSTER_ID"));
+    String connectionString =
+        String.format(
+            "mongodb+srv://%s:%s@%s.mongodb.net/?retryWrites=true&w=majority",
+            System.getenv("MONGODB_USERNAME"),
+            System.getenv("MONGODB_PASSWORD"),
+            System.getenv("MONGODB_CLUSTER_ID"));
 
     ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
-    MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(new ConnectionString(connectionString)).serverApi(serverApi).build();
+    MongoClientSettings settings =
+        MongoClientSettings.builder()
+            .applyConnectionString(new ConnectionString(connectionString))
+            .serverApi(serverApi)
+            .build();
 
     MongoClient client = MongoClients.create(settings);
     ping(client);
@@ -42,14 +50,14 @@ public class Mongodb {
 
     listDatabases(client);
 
-    //deleteOurDatabase(client, DB_NAME);
+    // deleteOurDatabase(client, DB_NAME);
 
     printSomeZips(client);
     printSomePeople(client, DB_NAME, COLLECTION_NAME, 100);
 
-//        insertOneDocument(client, DB_NAME, COLLECTION_NAME, new Document().append("name", "bob").append("town", "da bucket"));
+    //        insertOneDocument(client, DB_NAME, COLLECTION_NAME, new Document().append("name",
+    // "bob").append("town", "da bucket"));
     insertSomeDocuments(client, DB_NAME, COLLECTION_NAME, 10000);
-
   }
 
   void deleteOurDatabase(MongoClient client, String dbName) {
@@ -58,7 +66,6 @@ public class Mongodb {
 
   void deleteAllDocuments(MongoClient client, String dbName, String collection) {
     client.getDatabase(dbName).getCollection(collection).deleteMany(Filters.exists("name"));
-
   }
 
   void insertOneDocument(MongoClient client, String dbName, String collection, Document doc) {
@@ -70,13 +77,11 @@ public class Mongodb {
     MongoDatabase database = client.getDatabase("admin");
     Document d = database.runCommand(new Document("ping", 1));
     System.out.println("Pinged your deployment. You successfully connected to MongoDB!" + d);
-
   }
 
   void listDatabases(MongoClient client) {
     ListDatabasesIterable<Document> dbs = client.listDatabases();
     dbs.forEach(db -> System.out.println("db " + db.toString()));
-
   }
 
   void listDatabasesAndCollections(MongoClient client) {
@@ -84,7 +89,6 @@ public class Mongodb {
     for (Document doc : dbs.authorizedDatabasesOnly(true)) {
       MongoDatabase db = client.getDatabase(doc.get("name").toString());
       ListCollectionsIterable<Document> colls = db.listCollections();
-
     }
   }
 
@@ -93,15 +97,26 @@ public class Mongodb {
     MongoDatabase db = client.getDatabase(dbName);
 
     for (int i = 0; i < numDocs; i++) {
-      db.getCollection(collection).insertOne(new Document().append("name", faker.name().name()).append("town", faker.address().city()).append("phone", faker.phoneNumber().phoneNumber()).append("email", faker.internet().emailAddress()).append("catname", faker.cat().name()));
-
+      db.getCollection(collection)
+          .insertOne(
+              new Document()
+                  .append("name", faker.name().name())
+                  .append("town", faker.address().city())
+                  .append("phone", faker.phoneNumber().phoneNumber())
+                  .append("email", faker.internet().emailAddress())
+                  .append("catname", faker.cat().name()));
     }
   }
 
   void printSomePeople(MongoClient client, String dbName, String collection, int count) {
-    FindIterable<Document> ordered = client.getDatabase(dbName).getCollection(collection).find().limit(count).sort(ascending("name"));
+    FindIterable<Document> ordered =
+        client
+            .getDatabase(dbName)
+            .getCollection(collection)
+            .find()
+            .limit(count)
+            .sort(ascending("name"));
     ordered.forEach(d -> System.out.println("printSomePeople: " + d.toString()));
-
   }
 
   void printSomeZips(MongoClient client) {
@@ -110,15 +125,13 @@ public class Mongodb {
     // select matching documents....
     FindIterable<Document> zips = db.getCollection("zips").find(eq("city", "BROOKLYN"));
     zips.forEach(z -> System.out.println("printSomeZips: " + z.toString()));
-
   }
 
   void printAllZips(MongoClient client, int count) {
     // specify database...
     // sqlect matching documents....
-    FindIterable<Document> zips = client.getDatabase("sample_training").getCollection("zips").find(eq("city", "BROOKLYN"));
+    FindIterable<Document> zips =
+        client.getDatabase("sample_training").getCollection("zips").find(eq("city", "BROOKLYN"));
     zips.forEach(z -> System.out.println("printAllZips: " + z.toString()));
-
   }
-
 }
