1b911a07282fc9a8a8cb81082204a2fb1186caab
Bob Plotts <robertcplotts@gmail.com>
2024-02-06 21:41:58 -0500

    Improve organization.

--- src/main/java/com/cottagecoders/Mongodb.java
+++ src/main/java/com/cottagecoders/Mongodb.java
@@ -1,69 +1,124 @@
 package com.cottagecoders;
 
 import com.github.javafaker.Faker;
-import com.mongodb.*;
-import com.mongodb.client.*;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.ListCollectionsIterable;
+import com.mongodb.client.ListDatabasesIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Sorts.ascending;
 
 public class Mongodb {
-    public static void main(String[] args) {
 
-        Mongodb mdb = new Mongodb();
-        mdb.run();
+  private static final String DB_NAME = "bobdb";
+  private static final String COLLECTION_NAME = "people";
+
+  public static void main(String[] args) {
+
+    Mongodb mdb = new Mongodb();
+    mdb.run();
+
+  }
+
+  void run() {
+    String connectionString = String.format("mongodb+srv://%s:%s@%s.mongodb.net/?retryWrites=true&w=majority", System.getenv("MONGODB_USERNAME"), System.getenv("MONGODB_PASSWORD"), System.getenv("MONGODB_CLUSTER_ID"));
+
+    ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
+    MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(new ConnectionString(connectionString)).serverApi(serverApi).build();
+
+    MongoClient client = MongoClients.create(settings);
+    ping(client);
+
+    deleteAllDocuments(client, DB_NAME, COLLECTION_NAME);
+
+    listDatabases(client);
+
+    //deleteOurDatabase(client, DB_NAME);
+
+    printSomeZips(client);
+    printSomePeople(client, DB_NAME, COLLECTION_NAME, 100);
+
+//        insertOneDocument(client, DB_NAME, COLLECTION_NAME, new Document().append("name", "bob").append("town", "da bucket"));
+    insertSomeDocuments(client, DB_NAME, COLLECTION_NAME, 10000);
+
+  }
+
+  void deleteOurDatabase(MongoClient client, String dbName) {
+    client.getDatabase(dbName).drop();
+  }
+
+  void deleteAllDocuments(MongoClient client, String dbName, String collection) {
+    client.getDatabase(dbName).getCollection(collection).deleteMany(Filters.exists("name"));
+
+  }
+
+  void insertOneDocument(MongoClient client, String dbName, String collection, Document doc) {
+    client.getDatabase(dbName).getCollection(collection).insertOne(doc);
+  }
+
+  void ping(MongoClient client) {
+    // Send a ping to confirm a successful connection
+    MongoDatabase database = client.getDatabase("admin");
+    Document d = database.runCommand(new Document("ping", 1));
+    System.out.println("Pinged your deployment. You successfully connected to MongoDB!" + d);
+
+  }
+
+  void listDatabases(MongoClient client) {
+    ListDatabasesIterable<Document> dbs = client.listDatabases();
+    dbs.forEach(db -> System.out.println("db " + db.toString()));
+
+  }
+
+  void listDatabasesAndCollections(MongoClient client) {
+    ListDatabasesIterable<Document> dbs = client.listDatabases();
+    for (Document doc : dbs.authorizedDatabasesOnly(true)) {
+      MongoDatabase db = client.getDatabase(doc.get("name").toString());
+      ListCollectionsIterable<Document> colls = db.listCollections();
 
     }
+  }
 
-    void run() {
-        String connectionString = String.format("mongodb+srv://%s:%s@%s.mongodb.net/?retryWrites=true&w=majority", System.getenv("MONGODB_USERNAME"), System.getenv("MONGODB_PASSWORD"), System.getenv("MONGODB_CLUSTER_ID"));
+  void insertSomeDocuments(MongoClient client, String dbName, String collection, int numDocs) {
+    Faker faker = new Faker();
+    MongoDatabase db = client.getDatabase(dbName);
 
-        ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
-        MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(new ConnectionString(connectionString)).serverApi(serverApi).build();
-
-        // Create a new client and connect to the server
-        MongoClient mongoClient = MongoClients.create(settings);
-        // Send a ping to confirm a successful connection
-        MongoDatabase database = mongoClient.getDatabase("admin");
-        database.runCommand(new Document("ping", 1));
-
-        System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-
-        // specify database...
-        database = mongoClient.getDatabase("sample_training");
-
-        // sqlect matching documents....
-        FindIterable<Document> zips = database.getCollection("zips").find(eq("city", "BROOKLYN"));
-        zips.forEach(z -> System.out.println(" zip " + z.toString()));
-
-        // specify database
-        database = mongoClient.getDatabase("bobdb");
-        // add a row.
-        database.getCollection("people").insertOne(new Document().append("name", "bob").append("town", "da bucket"));
-
-        Faker faker = new Faker();
-        for (int i = 0; i < 10000; i++) {
-         //   database.getCollection("people").insertOne(new Document().append("name", faker.funnyName().name()).append("town", faker.address().city()));
-        }
-
-        FindIterable<Document> ordered = database.getCollection("people").find().limit(100).sort(ascending("name"));
-        ordered.forEach(d -> System.out.println( "ordered " + d.toString()));
-
-        try {
-            ListDatabasesIterable<Document> dbs = mongoClient.listDatabases();
-            dbs.forEach(db -> System.out.println("db " + db.toString()));
-
-            for (Document doc : dbs) {
-                System.out.println("---------");
-                MongoDatabase db = mongoClient.getDatabase(doc.get("name").toString());
-                ListCollectionsIterable<Document> colls = db.listCollections();
-                colls.forEach(c -> System.out.println("coll " + c.toString()));
-            }
-
-        } catch (MongoException e) {
-            e.printStackTrace();
-        }
+    for (int i = 0; i < numDocs; i++) {
+      db.getCollection(collection).insertOne(new Document().append("name", faker.name().name()).append("town", faker.address().city()).append("phone", faker.phoneNumber().phoneNumber()).append("email", faker.internet().emailAddress()).append("catname", faker.cat().name()));
 
     }
+  }
+
+  void printSomePeople(MongoClient client, String dbName, String collection, int count) {
+    FindIterable<Document> ordered = client.getDatabase(dbName).getCollection(collection).find().limit(count).sort(ascending("name"));
+    ordered.forEach(d -> System.out.println("printSomePeople: " + d.toString()));
+
+  }
+
+  void printSomeZips(MongoClient client) {
+    // specify database...
+    MongoDatabase db = client.getDatabase("sample_training");
+    // select matching documents....
+    FindIterable<Document> zips = db.getCollection("zips").find(eq("city", "BROOKLYN"));
+    zips.forEach(z -> System.out.println("printSomeZips: " + z.toString()));
+
+  }
+
+  void printAllZips(MongoClient client, int count) {
+    // specify database...
+    // sqlect matching documents....
+    FindIterable<Document> zips = client.getDatabase("sample_training").getCollection("zips").find(eq("city", "BROOKLYN"));
+    zips.forEach(z -> System.out.println("printAllZips: " + z.toString()));
+
+  }
+
 }
